dba1138ad4d12944306bf315c15a2ec3553d2bc7
Kullaying-P <6010110039@psu.ac.th>
2019-09-23 00:37:22 +0700

    add new word

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
-const word = ['hello','world'];
+const word = ['rose','lily','tulip','jasmine','orchid','daisy','lotus','peony','sunflower'];
 class App extends Component {
     render() {
         return (
