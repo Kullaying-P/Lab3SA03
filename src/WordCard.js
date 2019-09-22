84f036fe76ae7e831e54d4d37a1d39fda10d5d32
Kullaying-P <6010110039@psu.ac.th>
2019-09-22 23:48:54 +0700

    init commit

---
+++ src/WordCard.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import CharacterCard from './CharacterCard';
+import _ from 'lodash';
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+      word,
+      chars,
+      attempt: 1,
+      guess: [],
+      completed: false
+    }
+}
+export default class WordCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    } 
+    activationHandler = c => { 
+        let guess = [...this.state.guess, c]
+        this.setState({guess})
+        if(guess.length == this.state.chars.length){
+            if(guess.join('').toString() == this.state.word){
+                this.serState({guess: [], completed: true})
+            }else{
+                this.setState({guess: [], attempt: this.state.attempt + 1})
+            }
+        }
+    }
+    render() {
+        return ( 
+            <div> {
+                this.state.chars.map((c, i) => <CharacterCard value = {c} key = {i} activationHandler={this.activationHandler}/>) 
+                }  
+            </div >
+        );
+    }
+}
